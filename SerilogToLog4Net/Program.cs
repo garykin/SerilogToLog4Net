ebc4053c1ba56ea65a02362e41146d233aabcab4
kirill.garykin <kirill.garykin@gmail.com>
2017-05-18 10:49:38 +0300

    Initial commit

---
+++ SerilogToLog4Net/Program.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerilogToLog4Net
+{
+    public class Program
+    {
+        private static ILog _file_SF_Logger;
+        //   private static ILog _file_SF_Logger2;
+        private static ILogger _logger;
+        static void Main(string[] args)
+        {
+            // _file_SF_Logger = LogManager.GetLogger("File_SF_Logger");
+            log4net.Config.XmlConfigurator.Configure();
+            //_file_SF_Logger.Info("Log4Net created");
+
+            Log.Logger = new LoggerConfiguration()
+            .ReadFrom.AppSettings()
+           .MinimumLevel.Debug()
+           // .Enrich.WithProperty("SourceContext", System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString())
+           .WriteTo.Log4Net()
+           .CreateLogger();
+
+            _logger = Log.Logger;
+
+
+            //_file_SF_Logger = LogManager.GetLogger("File_SF_Logger");
+            //log4net.Config.XmlConfigurator.Configure();
+            //_file_SF_Logger.Info("Log4Net created");
+
+
+            _logger.Information("Serilog created");
+
+        }
+    }
+}
